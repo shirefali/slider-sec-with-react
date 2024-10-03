790a5d2dc6c65a3a925ace982339ea0ef25d970c
Shiref A.katheer❤🦅 <o@gmail.com>
2024-10-03 03:11:55 +0300

    commit reviews menu

---
+++ src/data.js
@@ -0,0 +1,32 @@
+const reviews = [
+    {
+        id: 1,
+        name: 'john smith',
+        job: 'web developer',
+        image: 'https://fastly.picsum.photos/id/661/2508/1672.jpg?hmac=-uhDMVp7FBXAhHNGOaSkVH2Re_C61ZdAlAOD6s6aZIw',
+        text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    },
+    {
+        id: 2,
+        name: 'peyan johnson',
+        job: 'web designer',
+        image: 'https://fastly.picsum.photos/id/669/4869/3456.jpg?hmac=g-4rQWsPdHoLi5g6ahHlvjKubSQzR-D9m7-WtblbmyM',
+        text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+    },
+    {
+        id: 3,
+        name: 'peter jones',
+        job: 'intern',
+        image: 'https://www.course-api.com/images/people/person-4.jpeg',
+        text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+    },
+    {
+        id: 4,
+        name: 'bill anderson',
+        job: 'the boss',
+        image: 'https://www.course-api.com/images/people/person-3.jpeg',
+        text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+    },
+];
+
+export default reviews;
